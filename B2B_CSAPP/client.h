b3382a9db4c7d0d433b7f5abdb8a701b20867024
lee-jonggu <110433092+lee-jonggu@users.noreply.github.com>
2022-10-27 22:40:42 +0900

    Update client.h

--- B2B_CSAPP/client.h
+++ B2B_CSAPP/client.h
@@ -8,20 +8,20 @@ class Client : public QTreeWidgetItem
 public:
     explicit Client(int id=0, QString="", QString="", QString="", QString="");  // id,name,address,phone num, type
 
-    int id() const;
-    QString getName() const;
-    void setName(QString&);
+    int id() const;                                 // 클라이언트 id
+    QString getName() const;                        // 클라이언트 이름 가져오기
+    void setName(QString&);                         // 클라이언트 이름 설정
 
-    QString getAddress() const;
-    void setAddress(QString&);
+    QString getAddress() const;                     // 클라이언트 주소 가져오기
+    void setAddress(QString&);                      // 클라이언트 주소 설정
 
-    QString getPhoneNum() const;
-    void setPhoneNum(QString&);
+    QString getPhoneNum() const;                    // 클라이언트 전화번호 가져오기
+    void setPhoneNum(QString&);                     // 클라이언트 전화번호 설정
 
-    QString getType() const;
-    void setType(QString&);
+    QString getType() const;                        // 클라이언트 타입 가져오기
+    void setType(QString&);                         // 클라이언트 타입 설정
 
-    bool operator==(const Client &other) const;
+    bool operator==(const Client &other) const;     // 클라이언트 비교 연산자 오버로딩
 };
 
 #endif // CLIENT_H
