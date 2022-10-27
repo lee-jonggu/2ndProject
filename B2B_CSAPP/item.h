e6429c9be6977baaf8d6fd29d6d4fadc85bf1a78
lee-jonggu <110433092+lee-jonggu@users.noreply.github.com>
2022-10-27 23:58:20 +0900

    Update item.h

--- B2B_CSAPP/item.h
+++ B2B_CSAPP/item.h
@@ -7,26 +7,25 @@ class Item : public QTreeWidgetItem
 {
 public:
 
-    explicit Item(int id=0, QString="", QString="", QString="", QString="", QString="");
-    // id,name,categori,color,stock,price
+    explicit Item(int id=0, QString="", QString="", QString="", QString="", QString="");    // id,name,categori,color,stock,price 
 
-    int id() const;
-    QString getName() const;
-    void setName(QString&);
+    int id() const;                                                                         // 아이템 id
+    QString getName() const;                                                                // 아이템 이름 가져오기
+    void setName(QString&);                                                                 // 아이템 이름 설정
 
-    QString getCategori() const;
-    void setCategori(QString&);
+    QString getCategori() const;                                                            // 아이템 카테고리 가져오기
+    void setCategori(QString&);                                                             // 아이템 카테고리 설정
 
-    QString getColor() const;
-    void setColor(QString&);
+    QString getColor() const;                                                               // 아이템 색상 가져오기
+    void setColor(QString&);                                                                // 아이템 색상 설정
 
-    QString getStock() const;
-    void setStock(QString&);
+    QString getStock() const;                                                               // 아이템 재고량 가져오기
+    void setStock(QString&);                                                                // 아이템 재고량 설정
 
-    QString getPrice() const;
-    void setPrice(QString&);
+    QString getPrice() const;                                                               // 아이템 가격 가져오기
+    void setPrice(QString&);                                                                // 아이템 가격 설정
 
-    bool operator==(const Item &other) const;
+    bool operator==(const Item &other) const;                                               // 아이템 간 비교 
 };
 
 #endif // ITEM_H
